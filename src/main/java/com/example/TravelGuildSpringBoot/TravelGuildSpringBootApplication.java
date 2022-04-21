a83db982e71824e61f5cc61f6cffa4021c5493dc
MaDu2116 <100189480+MaDu2116@users.noreply.github.com>
2022-04-21 21:43:59 +0700

    add

---
+++ src/main/java/com/example/TravelGuildSpringBoot/TravelGuildSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.TravelGuildSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelGuildSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelGuildSpringBootApplication.class, args);
+	}
+
+}
